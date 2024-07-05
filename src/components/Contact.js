440daf0a069379a2fe871d62d84427ed476c37dc
bpnt <pant.bini@gmail.com>
2024-07-05 20:54:29 +0545

    add:Added new sections

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {motion} from 'framer-motion';
+import {fadeIn} from '../variants';
 
 const Contact = () => {
   return <div className='section' id='contact'>Contact</div>;
